c5f7a7413644a580bdc8269d68f81a8a451f8bf9
Aidan Hamilton <AidanHamilton@aidans-mbp.domain>
2017-10-04 00:53:36 -0700

    Initial Commit

---
+++ FEM/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  FEM
+//
+//  Created by Aidan Hamilton on 10/4/17.
+//  Copyright © 2017 Aidan Hamilton. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
